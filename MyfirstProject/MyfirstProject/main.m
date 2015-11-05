4fa0015b13ef41c1e5cdfb98e474d9ad54d9b90a
Aniruddh Gokhale <gokhale.an@gmail.com>
2015-11-05 19:46:22 +0530

    first commit to ios branch

---
+++ MyfirstProject/MyfirstProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyfirstProject
+//
+//  Created by GDB Consultants on 02/11/15.
+//  Copyright (c) 2015 GDB Consultants. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSLog(@"welcome to your first program");
+    }
+    return 0;
+}
